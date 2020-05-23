9887ec2c8fa553b3c2b78977eb16665ec83b7481
褚鹏飞 <540788769@qq.com>
2020-05-23 09:57:11 +0800

    feat: add 算法题目的编写

--- leetCode/21.合并两个有序链表.js
+++ leetCode/21.合并两个有序链表.js
@@ -1,4 +1,12 @@
 /*
+ * @Author: your name
+ * @Date: 2019-12-14 16:39:33
+ * @LastEditTime: 2020-05-21 18:38:54
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /leetCode/21.合并两个有序链表.js
+ */
+/*
  * @lc app=leetcode.cn id=21 lang=javascript
  *
  * [21] 合并两个有序链表
@@ -20,9 +28,6 @@
  * 
  * 
  */
-
- // 
-
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
@@ -30,12 +35,31 @@
  *     this.next = null;
  * }
  */
+// 这道题的解题知识中，存在一个知识点 链表
+// js 实现链表 https://www.cnblogs.com/EganZhang/p/6594830.html
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var mergeTwoLists = function(l1, l2) {
-    
+var mergeTwoLists = function (l1, l2) {
+  var mergedHead = {
+    val: -1,
+    next: null
+  },
+    crt = mergedHead;
+  while (l1 && l2) {
+    if (l1.val > l2.val) {
+      crt.next = l2;
+      l2 = l2.next;
+    } else {
+      crt.next = l1;
+      l1 = l1.next;
+    }
+    crt = crt.next;
+  }
+  crt.next = l1 || l2;
+
+  return mergedHead.next;
 };
 
