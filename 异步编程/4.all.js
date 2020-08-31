7d308ce6a1c2e61d2eeb74c6b517c8345bd21e24
褚鹏飞 <540788769@qq.com>
2020-08-31 18:24:40 +0800

    feat: add all 方法

--- 异步编程/4.all.js
+++ 异步编程/4.all.js
@@ -1,1 +1,24 @@
-// let fs = require('fs')
+let fs = require('fs')
+
+function after(times, cb) {
+  let school = {};
+  return function (key, value) {
+    school[key] = value;
+    if (--times === 0) { // 如果达到次数，就执行after方法的回调函数
+      cb(school)
+    }
+  }
+}
+
+let out = after(2, function (result) {
+  console.log(result);
+})
+
+// 读取name
+fs.readFile('./name.txt', function (err, data) {
+  out('name', 'louis');
+})
+// 读取age
+fs.readFile('./age.txt', function (err, data) {
+  out('age', 10);
+})
