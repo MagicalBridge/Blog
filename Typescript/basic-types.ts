1a2eb0060c665b2b8a0f1904a87a97c192ef8c9a
褚鹏飞 <540788769@qq.com>
2020-10-13 17:43:34 +0800

    feat: 添加ts基础类型

---
+++ Typescript/basic-types.ts
@@ -0,0 +1,14 @@
+let isDone: boolean = false;
+
+let age: number = 20;
+
+let binaryNumber: number = 0b1111
+
+let firstName: string = 'louis'
+let message: string = `hello ${firstName},age is ${age}`
+
+let u:undefined = undefined;
+let n:null = null;
+
+// 
+let num:number = undefined;
