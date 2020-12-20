4a9458f094e2c20b57ae9635d5f96c35b76f3d33
褚鹏飞 <540788769@qq.com>
2020-12-20 19:52:50 +0800

    feat: add promise实现

--- promse-ts/src/index.js
+++ promse-ts/src/index.js
@@ -1,38 +1,5 @@
-class Subject {
-  constructor(name) {
-    this.name = name;
-    this.state = '我现在很开心'
-    this.observers = [];
-  }
-  // 传入 观测我的人
-  attach(o) {
-    this.observers.push(o);
-  }
-  setState(newVal) {
-    this.state = newVal;
-    this.observers.forEach(o => o.update(this));
-  }
-}
+// promise 可以解决多个异步并行执行
 
-class Observer {
-  constructor(name) {
-    this.name = name;
-  }
-
-  update(baby) {
-    console.log(baby.name + '对' + this.name + '说' + baby.state)
-  }
-}
-
-// 我家有个小宝宝 我和我媳妇就是观察者
-let baby = new Subject('小宝宝')
-
-// 两个实例
-let o1 = new Observer('爸爸')
-let o2 = new Observer('妈妈')
-
-baby.attach(o1)
-baby.attach(o2)
-
-baby.setState('我不开心了')
-
+new Promise((resolve, reject) => {
+  
+})
