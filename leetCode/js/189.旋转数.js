494922e675a06db52e5baf92461183d0446aac14
褚鹏飞 <540788769@qq.com>
2021-01-05 19:15:55 +0800

    添加189题目的第二种解法

--- leetCode/js/189.旋转数.js
+++ leetCode/js/189.旋转数.js
@@ -1,19 +1,10 @@
-var rotate = function (nums, k) {
-  // 设置两个变量
-  let temp;
-  let previous;
-  for (let i = 0; i < k; i++) {
-    // 这个部分取出的数组的最后一个元素 作为previous
-    previous = nums[nums.length - 1];
-    // 内层循环 
-    for (let j = 0; j < nums.length; j++) {
-      // 将当前循环到的元素赋值给一个中间变量 利用中间变量不断地
-      // 将前一个元素覆盖后面一个元素，这样循环完一轮，一个元素已经
-      // 翻转完毕
-      temp = nums[j];
-      nums[j] = previous;
-      previous = temp;
-    }
+function rotate(nums, k) {
+  let a = []; // 
+  for(let i = 0; i < nums.length; i++) {
+    a[(i+k) % nums.length] = nums[i];
+  }
+  for(let i = 0; i < nums.length; i++) {
+    nums[i] = a[i];
   }
 }
 // 数组
