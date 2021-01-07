d852608ddcfa03b1ed2df60a6302223b9913f7d6
褚鹏飞 <540788769@qq.com>
2021-01-07 10:16:36 +0800

    又完成了一遍promsie的书写

--- JavaScript-Base/promise/promise.js
+++ JavaScript-Base/promise/promise.js
@@ -1,56 +1,142 @@
-const PENDING = 'PENDING'; // 等待
-const RESOLVED = 'RESOLVED'; // 成功
-const REJECTED = 'REJECTED'; // 失败
+const PENDING = 'PENDING'; // 中间态
+const ONFULFILLED = 'ONFULFILLED'; // 成功
+const ONREJECTED = 'ONREJECTED' // 失败
+
+function resovlePromise(promise2, x, resolve, reject) {
+  // 自己等待完成是错误的实现，用一个类型错误结束掉promsie 2.3.1
+  if (promise2 === x) {
+    return reject(new TypeError('Chaining cycle detected for promise #<Promise>'))
+  }
+  // 只能调用一次
+  let called = false;
+  // 需要严格判断保证代码的一致性
+  if ((typeof x === 'object' && x !== null) || typeof x === 'function') {
+    try {
+      // 如果x是一个promise 是具有then方法的
+      let then = x.then;
+      // 如果 then 属性是个函数 这里我们就认为它是一个promsiel
+      if (typeof then === 'function') {
+        then.call(x, y => {
+          if (called) return;
+          called = true;
+          resovlePromise(promise2, y, resolve, reject);
+        }, r => {
+          if (called) return;
+          called = true;
+          reject(r)
+        })
+      } else {
+        // 如果then不是函数 还是普通值 那么 就直接resolve作为结果 2.3.3.4
+        resolve(x)
+      }
+    } catch (e) {
+      if (called) return;
+      called = true;
+      reject(e)
+    }
+  } else {
+    // 如果 x 是个普通值就直接返回 resolve 作为结果 2.3.4
+    resolve(x)
+  }
+}
 
 class Promise {
   constructor(executor) {
-    // 初始化时候状态是pending状态
-    this.status = PENDING;
-    // 成功的值
+    this.state = PENDING;
     this.value = undefined;
-    // 失败的原因
     this.reason = undefined;
+    this.onFulfilledCallback = []; // 成功回调的数组
+    this.onRejectedCallback = []; // 失败回调的数组 
 
-    // 成功函数(这种写法保证thiis指向)
+    // 成功状态执行的函数
     let resolve = (value) => {
-      // 为了防止调取reject之后再调取resolve 需要屏蔽
-      if (this.status === PENDING) {
+      if (this.state === PENDING) {
         this.value = value;
-        // 将当前的状态更改为 成功态
-        this.status = RESOLVED;
+        this.state = ONFULFILLED;
+        this.onFulfilledCallback.forEach(fn => fn());
       }
     }
-
-    // 失败函数
+    // 失败状态执行的函数
     let reject = (reason) => {
-      // 为了防止调取resolve之后再调取reject 需要屏蔽
-      if (this.status === PENDING) {
+      if (this.state === PENDING) {
         this.reason = reason;
-        // 将当前的状态更改为失败态
-        this.status = REJECTED;
+        this.state = ONREJECTED;
+        this.onRejectedCallback.forEach(fn => fn());
       }
     }
-    // 在执行构造器的时候有可能的情况是直接抛出错误，这种情况下，我们视为
-    // 失败，直接reject即可
+    // 立即执行函数
     try {
-      // 构造promise实例的时候传入立即执行函数
       executor(resolve, reject);
     } catch (e) {
-      reject(e);
+      reject(e)
     }
   }
-  // 每个promise实例都有一个then方法
-  // then方法我们在调用的时候传递进去了两个函数
-  // 一个是 onfulfilled onrejected
-  then(onfulfilled, onrejected) {
-    if (this.status === RESOLVED) {
-      onfulfilled(this.value);
-    }
+  // then方法接收两个函数onFulfilled 和 另一个函数
+  then(onFulfilled, onRejected) {
+    // 根据规范2.2.7中的表述可以知道,每调用一个then方法都会返回一个新的promsie
+    // 这样我们就能不停的进行then的链式调用
+    // 如何调用promsie的成功回调呢？
+    let promise2 = new Promise((resolve, reject) => {
+      if (this.state === ONFULFILLED) {
+        setTimeout(() => {
+          try {
+            // 用then的返回值 作为下一次的 then的 成功结果。
+            let x = onFulfilled(this.value);
+            resovlePromise(promise2, x, resolve, reject)
+          } catch (e) {
+            reject(e)
+          }
+        }, 0);
+      }
 
-    if (this.status === REJECTED) {
-      onrejected(this.reason);
-    }
+      if (this.state === ONREJECTED) {
+        setTimeout(() => {
+          try {
+            let x = onRejected(this.reason);
+            resovlePromise(promise2, x, resolve, reject)
+          } catch (e) {
+            reject(e)
+          }
+        }, 0);
+      }
+      // 如果是处于中间状态; 则将函数收集起来
+      if (this.state === PENDING) {
+        this.onFulfilledCallback.push(() => {
+          setTimeout(() => {
+            try {
+              let x = onFulfilled(this.value);
+              resovlePromise(promise2, x, resolve, reject)
+            } catch (e) {
+              reject(e)
+            }
+          }, 0);
+        })
+
+        this.onRejectedCallback.push(() => {
+          setTimeout(() => {
+            try {
+              let x = onRejected(this.reason);
+              resovlePromise(promise2, x, resolve, reject)
+            } catch (e) {
+              reject(e)
+            }
+          }, 0);
+        })
+      }
+    })
+    // 将这个promsie2返回出去
+    return promise2;
   }
+}
+
+// 在promsie 上面添加一个方法
+Promise.deferred = function () {
+  let dfd = {};
+  dfd.promise = new Promise((resolve, reject) => {
+    dfd.resolve = resolve;
+    dfd.reject = reject;
+  })
+  return dfd;
 }
 
 module.exports = Promise;
--- JavaScript-Base/promise/promise.md
+++ JavaScript-Base/promise/promise.md
@@ -571,6 +571,107 @@ export default Promise;
 onfulfilled 和 onrejected 必须是异步的。所以还需要抽出来一个定时器。包裹着这段代码。
 
 如何让这个 onFulfilled 和 onRejected 异步执行呢？这里使用一个 定时器去解决。
+```js
+const PENDING = 'PENDING'; // 中间态
+const ONFULFILLED = 'ONFULFILLED'; // 成功
+const ONREJECTED = 'ONREJECTED' // 失败
+
+class Promise {
+  constructor(executor) {
+    this.state = PENDING;
+    this.value = undefined;
+    this.reason = undefined;
+    this.onFulfilledCallback = []; // 成功回调的数组
+    this.onRejectedCallback = []; // 失败回调的数组 
+
+    // 成功状态执行的函数
+    let resolve = (value) => {
+      if (this.state === PENDING) {
+        this.value = value;
+        this.state = ONFULFILLED;
+        this.onFulfilledCallback.forEach(fn => fn());
+      }
+    }
+    // 失败状态执行的函数
+    let reject = (reason) => {
+      if (this.state === PENDING) {
+        this.reason = reason;
+        this.state = ONREJECTED;
+        this.onRejectedCallback.forEach(fn => fn());
+      }
+    }
+    // 立即执行函数
+    try {
+      executor(resolve, reject);
+    } catch (e) {
+      reject(e)
+    }
+  }
+  // then方法接收两个函数onFulfilled 和 另一个函数
+  then(onFulfilled, onRejected) {
+    // 根据规范2.2.7中的表述可以知道,每调用一个then方法都会返回一个新的promsie
+    // 这样我们就能不停的进行then的链式调用
+    // 如何调用promsie的成功回调呢？
+    let promise2 = new Promise((resolve, reject) => {
+      if (this.state === ONFULFILLED) {
+        setTimeout(() => {
+          try {
+            // 用then的返回值 作为下一次的 then的 成功结果。
+            let x = onFulfilled(this.value);
+            resolve(x)
+          } catch (e) {
+            reject(e)
+          }
+        }, 0);
+      }
+
+      if (this.state === ONREJECTED) {
+        setTimeout(() => {
+          try {
+            let x = onRejected(this.reason);
+            resolve(x)
+          } catch (e) {
+            reject(e)
+          }
+        }, 0);
+      }
+      // 如果是处于中间状态; 则将函数收集起来
+      if (this.state === PENDING) {
+        this.onFulfilledCallback.push(() => {
+          setTimeout(() => {
+            try {
+              let x = onFulfilled(this.value);
+              resolve(x)
+            } catch (e) {
+              reject(e)
+            }
+          }, 0);
+        })
+
+        this.onRejectedCallback.push(() => {
+          setTimeout(() => {
+            try {
+              let x = onRejected(this.reason);
+              resolve(x)
+            } catch (e) {
+              reject(e)
+            }
+          }, 0);
+        })
+      }
+    })
+
+    // 将这个promsie2返回出去
+    return promise2;
+  }
+}
+
+module.exports = Promise;
+```
+
+* 14) 核心逻辑 resolvePromsie 传递4个参数 promsie2 x resolve reject
+
+
 
 
 
