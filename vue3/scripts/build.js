85d187af1b0d68488583d625f8d7c493d01db2c1
褚鹏飞 <540788769@qq.com>
2021-03-03 12:50:40 +0800

    add: feat 添加rollup 构建配置文件

--- vue3/scripts/build.js
+++ vue3/scripts/build.js
@@ -3,7 +3,7 @@
 // 引入fs模块
 const fs = require('fs')
 // 引入子进程
-const execa = require('execa'); // 开启一个字进程
+const execa = require('execa'); // 开启一个字进程 最终还是使用rollup打包。
 // 使用readdirSync读取文件夹目录
 const targetArr = fs.readdirSync('packages')
 
@@ -18,7 +18,8 @@ const target = targetArr.filter(f => {
 
 // 我想要进行依次打包，并行打包
 async function build(target) {
-  console.log(target);
+  // execa 接收的参数 第一个参数 表示使用什么打包 -c 代表携带的参数  第三个参数 ：当子进程打包的信息共享给父进程
+  await execa('rollup', ['-c', '--environment', `TARGET:${target}`], { stdio: "inherit" })
 }
 
 function runParallel(targets, iteratorFn) {
