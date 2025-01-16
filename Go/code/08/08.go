9d65b07e82a4fb376d81cee8f48cbd86061e486f
chupengfei <450793585@qq.com>
2025-01-16 12:45:54 +0800

    feat: add interface type switch

---
+++ Go/code/08/08.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func hello() []string {
+	return nil
+}
+
+func GetValue() int {
+	return 1
+}
+
+func main() {
+	h := hello
+	if h == nil {
+		fmt.Println("nil")
+	} else {
+		fmt.Println("not nil")
+	}
+
+	// 编译失败
+	// i := GetValue()
+	// switch i.(type) {
+
+	// case int:
+	// 	fmt.Println("int")
+
+	// case string:
+	// 	fmt.Println("string")
+
+	// case interface{}:
+	// 	fmt.Println("interface")
+	// default:
+	// 	fmt.Println("unknown")
+	// }
+}
