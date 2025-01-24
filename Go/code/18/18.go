1c56eb7644f570fdd180cc788614e8845a3a65fb
chupengfei <450793585@qq.com>
2025-01-24 16:33:21 +0800

    fix: add 18 days

---
+++ Go/code/18/18.go
@@ -0,0 +1,23 @@
+package main
+
+func f1() (r int) {
+	defer func() {
+		r++
+	}()
+	return 0
+}
+
+func f2() (r int) {
+	t := 5
+	defer func() {
+		t = t + 5
+	}()
+	return t
+}
+
+func f3() (r int) {
+	defer func(r int) {
+		r = r + 5
+	}(r)
+	return 1
+}
