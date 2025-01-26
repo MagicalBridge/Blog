ca41b94f2ce85d6f588f77fa8963161ecca2a0af
chupengfei <450793585@qq.com>
2025-01-26 10:30:23 +0800

    fix: add 20 days

---
+++ Go/code/20/20.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	age int
+}
+
+func main() {
+	person := &Person{28}
+	defer fmt.Println(person.age)
+
+	defer func(p *Person) {
+		fmt.Println(p.age)
+	}(person)
+
+	defer func() {
+		fmt.Println(person.age)
+	}()
+
+	person = &Person{29}
+}
