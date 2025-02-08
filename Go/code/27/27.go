323485cb75aac9866bd7aa3346229e88904a64b0
chupengfei <450793585@qq.com>
2025-02-08 22:16:15 +0800

    fix: add 27 days demo case

--- Go/code/27/27.go
+++ Go/code/27/27.go
@@ -15,6 +15,17 @@ func (d Direction) String() string {
 	return [...]string{"North", "South", "East", "West"}[d]
 }
 
+type Math struct {
+	x, y int
+}
+
+var m = map[string]Math{
+	"foo": Math{2, 3},
+}
+
 func main() {
 	fmt.Println(South)
+
+	m["foo"].x = 4
+	fmt.Println(m["foo"].x)
 }
